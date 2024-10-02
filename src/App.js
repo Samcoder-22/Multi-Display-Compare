f321065cde750d0bd44e58cf1ae0f57cd2b4b50d
Sampath <149804533+Samcoder-22@users.noreply.github.com>
2024-10-02 09:00:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MultipleDisplays from './MultipleDisplays';
+
+function App() {
+  return (
+    <MultipleDisplays></MultipleDisplays>
+  );
+}
+
+export default App;
